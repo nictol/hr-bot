48d34196fa6385480a290f4848e6ff38f3bbe587
Nikita Tolkachev <n.tolkachev@unlimint.com>
2021-05-27 14:13:07 +0300

    Add message handlers and change readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# hr-bot
+# Description:
 This bot for education and demonstration only. Demo version
+
+
+# Quick start:
+1) Install python3 version after 3.7 
+2) Install Telegram Bot Api
+> <b>pip install pyTelegramBotAPI<b>
--- bot.py
+++ bot.py
@@ -1,40 +1,42 @@
-import pytz
 import telebot
 
 import config
 
-P_TIMEZONE = pytz.timezone(config.TIMEZONE)
-TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
-
 bot = telebot.TeleBot(config.TOKEN)
-bot.polling(none_stop=True)
 
 
 @bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.send_message(
-        message.chat.id,
-        'Greetings! I can show you exchange rates.\n' +
-        'To get the exchange rates press /exchange.\n' +
-        'To get help press /help.'
-    )
+def send_welcome(message):
+    bot.reply_to(message, "This is HR bot by Silvia Bobariko")
+    print('start command ' + str(message.chat.id))
 
-    @bot.message_handler(commands=['help'])
-    def help_command(message):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        keyboard.add(
-            telebot.types.InlineKeyboardButton(
-                'Message the developer', url='telegram.me/artiomtb'
-            )
-        )
-        bot.send_message(
-            message.chat.id,
-            '1) To receive a list of available currencies press /exchange.\n' +
-            '2) Click on the currency you are interested in.\n' +
-            '3) You will receive a message containing information regarding the source and the target currencies, ' +
-            'buying rates and selling rates.\n' +
-            '4) Click “Update” to receive the current information regarding the request. ' +
-            'The bot will also show the difference between the previous and the current exchange rates.\n' +
-            '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
-            reply_markup=keyboard
-        )
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Help Message hasn\'t implemented yet")
+    print('help command ' + str(message.chat.id))
+
+
+@bot.message_handler(commands=['contacts'])
+def contacts_command(message):
+    bot.send_message(message.chat.id, 'Акционерное общество «Силовые машины – ЗТЛ, ЛМЗ,'
+                                      'Электросила, Энергомашэкспорт» (АО «Силовые машины») \n' +
+                     'Россия, 195009, Санкт-Петербург \n' + 'ул. Ватутина, д.3, Лит. А \n' +
+                     '+7 (812) 346 70 37 \n' +
+                     '+7 (812) 346 70 35 \n' +
+                     'mail@power-m.ru \n')
+    print('contacts command ' + str(message.chat.id))
+
+
+@bot.message_handler(commands=['timetable'])
+def timetable_command(message):
+    bot.send_message(message.chat.id, 'Help Message hasn\'t implemented yet')
+    print('timetable command ' + str(message.chat.id))
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
