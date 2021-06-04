f58f30a6d6648165c9b93af4d5ff94b69c0b4da9
Nikita Tolkachev <n.tolkachev@unlimint.com>
2021-06-04 19:10:58 +0300

    Refactoring

--- config.py
+++ config.py
@@ -2,11 +2,9 @@ TOKEN = '1872863499:AAETY4Fvi66bcZGrTMHl-tpOjt5QJC_PzT4'
 TIMEZONE = 'Europe/Moscow'
 TIMEZONE_COMMON_NAME = 'Moscow'
 
-
 CONTACTS_COMMAND = 'contacts'
 CONTACTS_ANSWER_FILE = 'answers/contacts.txt'
 CONTACTS_LOG = 'Contacts command sent'
-
 
 HELP_COMMAND = 'help'
 HELP_ANSWER_FILE = 'answers/help.txt'
